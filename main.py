bcfa94ee7398bc84aa664abef5ed61fe2fca283f
Rom1994 <romariolp@gmail.com>
2024-07-27 23:30:21 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a - b)
+
+a = int(input( 'Digite o Ano atual ex. 2001'))
+b = int(input(' Agora digita o ano que nasceu: '))
+
+print(f'Se o ano atual é {a} e vc nasceu em {b} então você tem {sum(a, b)} anos de idade.')
